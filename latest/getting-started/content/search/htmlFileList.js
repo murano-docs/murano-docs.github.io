0341fb780d8c749f8e03af07af50d4bc00e0f356
tnurlygayanov <tnurlygayanov@mirantis.com>
2013-09-11 13:46:57 +0400

    generated Wed Sep 11 13:46:57 MSK 2013.

---
+++ latest/getting-started/content/search/htmlFileList.js
@@ -0,0 +1,25 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "ch05.html";
+fl["1"]= "ch04s01.html";
+fl["2"]= "ch03.html";
+fl["3"]= "ch02s01.html";
+fl["4"]= "ch03s03.html";
+fl["5"]= "ch04s04s04.html";
+fl["6"]= "ch04s04.html";
+fl["7"]= "ch04.html";
+fl["8"]= "ch02s01s01.html";
+fl["9"]= "ch02s01s02.html";
+fl["10"]= "ch03s02.html";
+fl["11"]= "ch01s01.html";
+fl["12"]= "ch04s04s02.html";
+fl["13"]= "ch03s01.html";
+fl["14"]= "ch06.html";
+fl["15"]= "ch02.html";
+fl["16"]= "ch01.html";
+fl["17"]= "ch04s04s03.html";
+fl["18"]= "ch04s03.html";
+fl["19"]= "ch05s01.html";
+fl["20"]= "ch04s02.html";
+fl["21"]= "ch04s04s05.html";
+fl["22"]= "ch04s04s01.html";
