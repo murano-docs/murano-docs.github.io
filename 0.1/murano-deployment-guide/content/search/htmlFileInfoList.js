8c2dacfb53fad365cbb479e12da9ecfdb210b7f9
tnurlygayanov <tnurlygayanov@mirantis.com>
2013-11-22 13:20:16 +0400

    generated Fri Nov 22 13:20:16 MSK 2013.

---
+++ 0.1/murano-deployment-guide/content/search/htmlFileInfoList.js
@@ -0,0 +1,16 @@
+fil = new Array();
+fil["0"]= "ch02s01s06.html@@@Convert image to QCOW2 format@@@null";
+fil["1"]= "ch03.html@@@Chapter 3. Install OpenStack (DevStack)@@@null";
+fil["2"]= "ch02s01.html@@@Prepare the system@@@null";
+fil["3"]= "ch03s03.html@@@Start devstack@@@null";
+fil["4"]= "ch04.html@@@Chapter 4. Log Files Locations@@@null";
+fil["5"]= "ch02s01s01.html@@@Install KVM@@@null";
+fil["6"]= "ch02s01s02.html@@@Install and configure SAMBA@@@null";
+fil["7"]= "ch03s02.html@@@Prepare the system for devstack installation@@@null";
+fil["8"]= "ch01s01.html@@@Software RAID Configuration@@@null";
+fil["9"]= "ch02s01s03.html@@@Get all the files@@@null";
+fil["10"]= "ch03s01.html@@@Prepare Windows Image@@@null";
+fil["11"]= "ch02.html@@@Chapter 2. Prepare Reference Windows Image@@@null";
+fil["12"]= "ch01.html@@@Chapter 1. System Configuration@@@null";
+fil["13"]= "ch02s01s05.html@@@Post-install configuration@@@null";
+fil["14"]= "ch02s01s04.html@@@Start VM installation@@@null";
