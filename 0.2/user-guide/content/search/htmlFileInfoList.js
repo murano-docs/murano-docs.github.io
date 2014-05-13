58c95a46ba87f3e054ba61577f65663b2be7e21d
murano-docs <murano-eng@mirantis.com>
2014-05-13 14:40:52 +0400

    generated Tue May 13 14:40:52 MSK 2014.

---
+++ 0.2/user-guide/content/search/htmlFileInfoList.js
@@ -0,0 +1,15 @@
+fil = new Array();
+fil["0"]= "ch02s02.html@@@Creating service prototype@@@null";
+fil["1"]= "ch02s02s05.html@@@ASP.NET Farm Service@@@null";
+fil["2"]= "ch02s02s03.html@@@Internet Information Web Farm Service@@@null";
+fil["3"]= "ch02s01.html@@@Creating environment@@@null";
+fil["4"]= "ch02s02s07.html@@@SQL Server Failover Cluster@@@null";
+fil["5"]= "ch02s03.html@@@Deploying environment@@@null";
+fil["6"]= "ch02s02s04.html@@@ASP.NET Service@@@null";
+fil["7"]= "ch02s02s01.html@@@Active Directory@@@null";
+fil["8"]= "ch01s01.html@@@Document change history@@@null";
+fil["9"]= "ch02s04.html@@@Detailed information@@@null";
+fil["10"]= "ch02s02s06.html@@@MS SQL Service@@@null";
+fil["11"]= "ch02s02s02.html@@@Internet Information Service@@@null";
+fil["12"]= "ch02.html@@@Chapter 2. Murano dashboard plugin@@@null";
+fil["13"]= "ch01.html@@@Chapter 1. How can I use Murano Service?@@@null";
